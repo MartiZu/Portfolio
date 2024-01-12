f19dc336560e353c052d1a7b5d37313590bbd6c9
MartiZu <martina.zurli@gmail.com>
2024-01-12 23:17:24 +0100

    added functionality

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,3 +1,3 @@
 export default function Footer() {
-  return <div className="footer container"></div>;
+  return <div className="footer container">Made with love &copy; || Martina Zurli - 2024</div>;
 }
